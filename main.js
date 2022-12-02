073b53fa07b9bc0fb2ec25ce4bbb366e2edc9cd5
Jason Fritsche <fritsche.jason@gmail.com>
2022-12-01 20:35:57 -0600

    create array containing all show dates

--- main.js
+++ main.js
@@ -5,11 +5,29 @@ async function scrape() {
   const page = await browser.newPage();
 
   await page.goto("https://austin.showlists.net/");
-  var element = await page.waitForSelector(
-    ".entry-content > div:nth-child(1) > a"
-  );
-  const text = await page.evaluate((element) => element.textContent, element);
-  console.log(text);
+
+  // const element = await page.waitForSelector(
+  //   ".entry-content > div:nth-child(1) > a"
+  // );
+
+  // const text = await page.evaluate((element) => element.textContent, element);
+  // console.log(text);
+  const shows = await page.evaluate(() => {
+    const showElements = document.querySelectorAll("div.showlist div");
+    const showsElementsArr = Array.from(showElements);
+    const showDatesArr = showsElementsArr.filter(
+      (el) => el.querySelector(".text-brand")?.innerHTML
+    );
+
+    const _shows = showDatesArr.map((el) => {
+      return {
+        date: el.querySelector(".text-brand")?.innerHTML,
+      };
+    });
+    return _shows;
+  });
+
+  console.log(shows);
   await browser.close();
 }
 scrape();
