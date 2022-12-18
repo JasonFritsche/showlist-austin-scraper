360b096cacd7cba10c618410ffdbc838eca91f5b
Jason Fritsche <fritsche.jason@gmail.com>
2022-12-17 22:00:27 -0600

    map show description and href

--- main.js
+++ main.js
@@ -1,33 +1,48 @@
 const puppeteer = require("puppeteer");
 
 async function scrape() {
-  const browser = await puppeteer.launch({});
+  const browser = await puppeteer.launch({ dumpio: true });
   const page = await browser.newPage();
 
   await page.goto("https://austin.showlists.net/");
-
-  // const element = await page.waitForSelector(
-  //   ".entry-content > div:nth-child(1) > a"
-  // );
-
-  // const text = await page.evaluate((element) => element.textContent, element);
-  // console.log(text);
-  const shows = await page.evaluate(() => {
+  const allShowData = await page.evaluate(() => {
     const showElements = document.querySelectorAll("div.showlist div");
     const showsElementsArr = Array.from(showElements);
     const showDatesArr = showsElementsArr.filter(
       (el) => el.querySelector(".text-brand")?.innerHTML
     );
 
-    const _shows = showDatesArr.map((el) => {
+    const parseShows = (element) => {
+      const liElements = [...element.childNodes];
+
+      const liData = liElements
+        .filter((node) => node.nodeType === Node.ELEMENT_NODE)
+        .map((el, idx) => {
+          const childNodeArr = Array.from(el.childNodes);
+          const showTitleEl = childNodeArr.find((childNode) =>
+            childNode?.className?.includes("show-title")
+          );
+          return {
+            description: showTitleEl?.innerText,
+            showHref: showTitleEl?.href,
+            childNodeArr: childNodeArr,
+          };
+        });
+      return liData;
+    };
+
+    const _allShowData = showDatesArr.map((el) => {
       return {
-        date: el.querySelector(".text-brand")?.innerHTML,
+        data: el.childNodes,
+        shows: parseShows(el.querySelector("div.show-date > ul")),
       };
     });
-    return _shows;
+    return _allShowData;
   });
 
-  console.log(shows);
+  //console.log(allShowData);
+  page.on("console", (msg) => console[msg._type]("PAGE LOG:", msg._text));
+
   await browser.close();
 }
 scrape();
