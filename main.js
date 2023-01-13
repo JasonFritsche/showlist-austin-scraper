fee0320bb33bcae7405268bfe5cf8e59504554c4
Jason Fritsche <fritsche.jason@gmail.com>
2023-01-12 18:43:31 -0600

    feat: created scraper firebase function

--- main.js
+++ main.js
@@ -5,4 +5,5 @@ const shows = await scrape();
 if (shows?.length) {
   // update firebase (overwrite everything)
   updateDb({ shows: shows });
-}
+  // https://firebase.google.com/docs/functions/get-started#deploy-functions-to-a-production-environment
+
