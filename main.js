7a3281ba09bb7851aa76f1fc6e14b5ad4e291553
Jason Fritsche <fritsche.jason@gmail.com>
2023-01-25 20:44:51 -0600

    return data in main.js

--- main.js
+++ main.js
@@ -2,8 +2,4 @@ import { updateDb } from "./firebase.js";
 import scrape from "./scrape.js";
 
 const shows = await scrape();
-if (shows?.length) {
-  // update firebase (overwrite everything)
-  updateDb({ shows: shows });
-  // https://firebase.google.com/docs/functions/get-started#deploy-functions-to-a-production-environment
-
+return shows;
