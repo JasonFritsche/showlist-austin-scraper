ed601e7edb4d4def236a8339539c4c0a9869eefb
Jason Fritsche <fritsche.jason@gmail.com>
2023-01-25 21:34:12 -0600

    moved scrape function to main file

--- main.js
+++ main.js
@@ -1,4 +1,27 @@
-const scrape = require("./scrape.js");
+async function scrape() {
+  const browser = await puppeteer.launch({ dumpio: true });
+  const page = await browser.newPage();
+
+  await page.goto("https://austin.showlists.net/");
+  const allShowData = await page.evaluate(() =>
+    Array.from(document.querySelectorAll(".show-date"), (e) => ({
+      date: e.querySelector("h5.text-brand").innerText,
+      shows: Array.from(e.querySelectorAll("li.showlist-item"), (el) => ({
+        description: el.querySelector(".show-title")?.innerText,
+        eventLink: el.querySelector(".show-title")?.href,
+        venue: {
+          title: el.querySelector(".venue-title")?.innerText,
+          href: el.querySelector(".venue-title")?.href,
+          map: el.querySelector(".maps-link")?.href,
+        },
+        time: el.querySelector(".text-gray")?.innerText,
+      })),
+    }))
+  );
+
+  await browser.close();
+  return allShowData;
+}
 
 const shows = await scrape();
 return shows;
